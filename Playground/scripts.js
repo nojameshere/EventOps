d1eb3832c4177a5cf861ba4e8aac7e7959101439
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-06-21 19:15:46 -0600

    Calculator completion
    The calculator is completed, I still have to plug the code into the site and make it look pretty. I might also make it a function and add a button to calculate instead of on a keyup. We'll see how much I like it.

--- Playground/scripts.js
+++ Playground/scripts.js
@@ -2,7 +2,7 @@
 //Number of attendees * ([2.5 0-500],[1.5 501 - 1000],[1.15 1001 - 2500],[.9 2501 - 5000],[.6 5000+] )
 
 let form = document.querySelector('.calculator');
-let attendees = 0;  //number of attendees imput in calc
+let total = document.querySelector('.cost');
 let attendeeCalc = 0; //calculated based on number of attendees
 let apNumber = 0; //number of APs in calc
 let apCalc = 0; //calculated based on number of APs
@@ -12,19 +12,47 @@ let support = form.getElementsByClassName('.onsiteSupport').value;
 //This needs to go inside of an event listener for clicks OR field changes (IE, when a user types)
 attendees = form.getElementsByClassName('.attendee').value;
 
-if(attendees < 501){
-    attendeeCalc = attendees * 2.5;
-    //2.5*
-}else if(attendees < 1001){
-    attendeeCalc = attendees * 1.5;
-    //1.5*
-}else if(attendees < 2501){
-    attendeeCalc = attendees * 1.15;
-    //1.15*
-}else if(attendees < 5001){
-    attendeeCalc = attendees * 0.9;
-    //.9*
-}else{
-    attendeeCalc = attendees * 0.6;x
-    //.6*
-}
+form.addEventListener('keyup', e => {
+    let attendees = document.querySelector('.attendee').value;
+    let accessPoint = document.querySelector('.accessPoint').value;
+    let checkbox = document.querySelector('.onsiteSupport');
+    let supportCost = 0
+    console.log(accessPoint);
+    if(attendees < 501){
+        attendeeCalc = attendees * 2.5;
+        if(checkbox.checked === true){
+            supportCost = 500;
+        }
+        //2.5*
+    }else if(attendees < 1001){
+        attendeeCalc = attendees * 1.5;
+        if(checkbox.checked === true){
+            supportCost = 500;
+        }
+        //1.5*
+    }else if(attendees < 2501){
+        attendeeCalc = attendees * 1.15;
+        if(checkbox.checked === true){
+            supportCost = 750;
+        }
+        //1.15*
+    }else if(attendees < 5001){
+        attendeeCalc = attendees * 0.9;
+        if(checkbox.checked === true){
+            supportCost = 1000;
+        }
+        //.9*
+    }else{
+        attendeeCalc = attendees * 0.6;
+        if(checkbox.checked === true){
+            supportCost = 2500;
+        }
+        //.6*
+    }
+    let apPreCalc = accessPoint * attendees;
+    apCalc = apPreCalc * 0.25;
+    let totalCost = apCalc + attendeeCalc + supportCost;
+    total.innerHTML = `$${totalCost}`;
+    console.log(totalCost);
+})
+
